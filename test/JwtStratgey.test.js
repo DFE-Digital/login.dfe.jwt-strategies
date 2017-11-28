8a5d4d57dc95b2af3908302a94647e13cee70363
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-11-28 21:51:26 +0000

    WIP test refactor

--- test/JwtStratgey.test.js
+++ test/JwtStratgey.test.js
@@ -1,16 +1,26 @@
 'use strict';
 
-const { expect } = require('chai');
-const {AssertionError} = require('assert');
-const sinon = require('sinon');
+jest.mock('adal-node', () => {
+  const AuthenticationContext = jest.fn().mockImplementation(() =>  {
+    return {
+      acquireTokenWithClientCredentials: () => new Promise((resolveNew) => resolveNew(
+        {accessToken: 'foo'})),
+    }});
+  return {
+    AuthenticationContext: jest.fn().mockImplementation(AuthenticationContext),
+    Logging: {
+      getLoggingOptions: jest.fn().mockImplementation(() => ({ level: '' })),
+      setLoggingOptions: jest.fn(),
+    },
+  };
+});
+const { AssertionError } = require('assert');
 const adal = require('adal-node');
 
 const expectedClientsToken = 'super-secret-super-token';
 const jwtStrategy = require('../src/');
 
 describe('When using the HotConfigApiAdapter with JWT config', () => {
-  let sandbox;
-
   const noJwtConfig = {
     hotConfig: {
       url: undefined,
@@ -37,6 +47,7 @@ describe('When using the HotConfigApiAdapter with JWT config', () => {
         clientId: 'some_client_id',
         clientSecret: 'client_secret',
         resource: 'some_resource',
+        logLevel: 'info',
       },
     },
   };
@@ -67,45 +78,42 @@ describe('When using the HotConfigApiAdapter with JWT config', () => {
     },
   };
 
-  beforeEach(() => {
-    sandbox = sinon.sandbox.create();
-  });
-
-  afterEach(() => {
-    sandbox.restore();
-  });
-
   it('does not use a jwt strategy', () => {
     const strategy = jwtStrategy(noJwtConfig.hotConfig);
-    expect(strategy).to.equal(null);
+    expect(strategy).toBe(null);
   });
 
   it('use the secret jwt strategy', async () => {
     const strategy = jwtStrategy(secretConfig.hotConfig);
     const actual = await strategy.getBearerToken();
-    expect(actual).to.equal(expectedClientsToken);
+    expect(actual).toBe(expectedClientsToken);
   });
 
   it('use the Active Directory jwt strategy', async () => {
     const strategy = jwtStrategy(jwtConfig.hotConfig);
-    sandbox.stub(adal.AuthenticationContext.prototype, 'acquireTokenWithClientCredentials').yields(null, { accessToken: 'foo' });
+    adal.AuthenticationContext.mockReset();
+    adal.AuthenticationContext.mockImplementation(() => ({
+      acquireTokenWithClientCredentials: () => ({ accessToken: 'foo' }),
+    }));
+
     const response = await strategy.getBearerToken();
-    expect(response).to.equal('foo');
+
+    expect(response).toBe('foo');
   });
 
   it('no aad strategy if the required config is not present', () => {
-    try{
-      jwtStrategy(brokenAadConfig.hotConfig)
-    } catch(e) {
-      expect(e).to.be.an.instanceof(AssertionError);
+    try {
+      jwtStrategy(brokenAadConfig.hotConfig);
+    } catch (e) {
+      expect(e).toBeInstanceOf(AssertionError);
     }
   });
 
   it('incompatible auth.type specified', () => {
-    try{
-      jwtStrategy(brokenAuthTypeConfig.hotConfig)
-    } catch(e) {
-      expect(e).to.be.an.instanceof(AssertionError);
+    try {
+      jwtStrategy(brokenAuthTypeConfig.hotConfig);
+    } catch (e) {
+      expect(e).toBeInstanceOf(AssertionError);
     }
-  })
+  });
 });
